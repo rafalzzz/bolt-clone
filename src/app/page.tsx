e6df2fe6c509a4e47991168800dea9a43465a8d0
rafalzzz <rzdeb94@gmail.com>
2024-12-03 21:17:47 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/features/navbar';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'></main>
+    </div>
+  );
+}
