85e3f4772f6d9b3b86c40900ed2b918d07dd13c7
rafalzzz <rzdeb94@gmail.com>
2024-12-10 16:06:53 +0100

    fix build error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document<TDocumentInitialProps> {
   static async getInitialProps(ctx: DocumentContext): Promise<TDocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
 
-    const cookies = ctx.req ? cookie.parse(ctx.req.headers.cookie || '') : {};
+    const cookies = ctx.req && ctx.req.headers.cookie ? cookie.parse(ctx.req.headers.cookie) : {};
     const darkMode = cookies[DARK_MODE] === EDarkMode.ENABLED;
 
     return { ...initialProps, darkMode };
