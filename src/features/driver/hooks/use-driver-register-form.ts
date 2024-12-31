b1af5073c3cd64d195a01bd9cfb7ac5a38a5e276
rafalzzz <rzdeb94@gmail.com>
2024-12-31 15:04:44 +0100

    add comment

--- src/features/driver/hooks/use-driver-register-form.ts
+++ src/features/driver/hooks/use-driver-register-form.ts
@@ -1,14 +1,5 @@
 const useDriverRegisterForm = () => {
-  /*   const initialState = {
-    data: {} as TSendDriverEmailSchema,
-    errors: {} as TSendDriverEmailSchemaError,
-  };
-
-  const [state, registerDriverAction] = useActionState(sendDriverEmail, initialState);
-
-  console.log('useDriverRegisterForm', { state });
-
-  return { registerDriverAction }; */
+  // TODO - add form logic
 };
 
 export default useDriverRegisterForm;
