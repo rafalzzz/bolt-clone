e93305648c2748032f40bae64540fe0b915f428b
rafalzzz <rzdeb94@gmail.com>
2025-02-19 20:23:10 +0100

    add initial logic to CustomFormFields component

--- src/features/driver/actions/register-driver.ts
+++ src/features/driver/actions/register-driver.ts
@@ -1,5 +1,6 @@
 'use server';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function registerDriver(formData: FormData) {
   // TODO - register user action
 
--- src/shared/components/custom-select/index.tsx
+++ src/shared/components/custom-select/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { useTranslations } from 'next-intl';
 import { Path, UseFormRegister, UseFormSetValue } from 'react-hook-form';
 import {
   ControlProps,
