e5bd7dc2502884fa93c4d0391d5711d94607e56e
rafalzzz <rzdeb94@gmail.com>
2025-03-15 19:37:41 +0100

    remove unused import

--- src/features/driver-registration/utils/handle-unique-columns-check.ts
+++ src/features/driver-registration/utils/handle-unique-columns-check.ts
@@ -2,8 +2,6 @@ import { supabase } from '@/lib/supabase';
 
 import CustomResponseError from '@/shared/classes/custom-response-error';
 
-import getErrorMessage from '@/shared/utils/common/get-error-message';
-
 import { EDriverRegistrationTokenPayloadKeys } from '@/features/driver-registration/enums/driver-registration-form-keys';
 import { EDriverEntityKeys } from '@/shared/enums/driver-entity-keys';
 
